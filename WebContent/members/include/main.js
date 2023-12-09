f4af6b7ba8ce83cb74a63fc9f7f6ee4dd712130a
전의진 <138093799+Jeonuijin@users.noreply.github.com>
2023-12-09 19:17:41 +0900

    Delete WebContent directory

--- WebContent/members/include/main.js
+++
@@ -1,9 +0,0 @@
-	function main() { // 메인
-		location.href="01_main.jsp";
-	}
-	function login() { // 로그인
-		location.href="02_login.jsp";
-	}
-	function signUp() { // 회원가입
-		location.href="03_signUp.jsp";
-	}
--- WebContent/members/include/main_login.js
+++
@@ -1,9 +0,0 @@
-	function main() { // 메인
-		location.href="01_main.jsp";
-	}
-	function myPage() { // 마이페이지
-		location.href="06_myPage.jsp";
-	}
-	function logout() { // 로그아웃
-		location.href="05_logout.jsp";
-	}
